57d753a628c68be29e827a61961ba7821fd7c6c4
AdiPat <adithemagnificent@gmail.com>
2012-09-05 18:34:36 +0530

    Edits to build script and change hostname..

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
-#!/bin/bash -x
-CYANOGENMOD=../../..
-
 # Make mrproper
+echo "Cleaning Source"
+
 make clean mrproper
 
 # Set config
+
+echo "Building Config"
+
 make latona_galaxysl_defconfig
 
 # Make modules
+
+echo "Compiling Modules"
+
 make -j8
 
 # Copy modules
 find -name '*.ko' -exec cp -av {} ../tools/out/system/lib/modules/ \;
+
+echo "Repacking the Kernel now"
+
 
 ./repack.sh 
 
