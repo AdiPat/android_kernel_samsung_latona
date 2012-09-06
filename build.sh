a4b72c5d5483ef4695a10a0479916bcfa05476ab
AdiPat <adithemagnificent@gmail.com>
2012-09-06 19:12:52 +0530

    Some modifications to my build script

--- build.sh
+++ build.sh
@@ -9,6 +9,12 @@ echo "Building Config"
 
 make titanium_defconfig
 
+echo "Edit build version"
+
+read vrsn;
+
+echo $vrsn > .version
+
 # Make modules
 
 echo "Compiling Modules"
@@ -22,5 +28,23 @@ echo "Repacking the Kernel now"
 
 cd ..
 
-./repack.sh 
+echo "Packing Kernel"
+
+rm tools/unpack/zImage
+cp Kernel/arch/arm/boot/zImage tools/unpack/zImage
+
+cd tools
+rm out/boot.img
+
+tools/mkbootimg --kernel unpack/zImage --ramdisk unpack/boot.img-ramdisk.gz -o out/boot.img --base 10000000
+
+cd out
+
+rm *.zip
+
+zip -r Titanium-Kernel$v.zip META-INF system boot.img
+
+cp Titanium-Kernel$v.zip ../../Titanium-Kernel$v.zip
+
+echo "Done"
 
