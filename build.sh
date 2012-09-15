a4207e6282c0981157d9107468cf78705bc26b27
AdiPat <adithemagnificent@gmail.com>
2012-09-15 22:32:39 +0530

    ARM: Add CMDLINE_EXTEND for UART, enable EARLY_PRINTK
    New defconfig with debugging options, since debugging stuff can sometimes slow down the Kernel
    
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash 
 #
 # Kernel Build script for GT-I9003 Custom Titanium Kernel 
@@ -122,6 +123,57 @@ find -name '*.ko' -exec cp -av {} $MODULES_DIR/ \;
 # Strip unneeded symbols
 cd $MODULES_DIR
 echo ">> Strip modules for size"
+=======
+# Make mrproper
+echo "Cleaning Source"
+echo "Building Config"
+
+make clean mrproper -j1000
+
+# Set config
+
+make latona_debug_defconfig -j1000
+
+echo "Edit build version"
+
+read vrsn;
+
+echo $vrsn > .version
+
+# Make modules
+
+echo "Compiling Modules and Kernel"
+
+rm ../compile.log
+
+make -j84 > ../compile.log 2>&1
+
+echo "Copy modules"
+
+find -name '*.ko' -exec cp -av {} ../tools/out/system/lib/modules/ \;
+
+echo "Repacking the Kernel now"
+
+cd ..
+
+rm *.zip
+
+echo "Packing Kernel"
+
+rm tools/unpack/zImage
+cp Kernel/arch/arm/boot/zImage tools/unpack/zImage
+
+cd tools
+rm out/boot.img
+
+tools/mkbootimg --kernel unpack/zImage --ramdisk unpack/boot.img-ramdisk.gz -o out/boot.img --base 10000000
+
+cd out
+
+cd system/lib/modules
+
+echo "Strip modules for size"
+>>>>>>> 58eadb2... ARM: Add CMDLINE_EXTEND for UART, enable EARLY_PRINTK
 
 for m in $(find . | grep .ko | grep './')
 do echo $m
