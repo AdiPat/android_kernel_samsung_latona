11be3da6647f41fe46cd93a37a6760ede68694d3
AdiPat <adithemagnificent@gmail.com>
2012-11-01 22:34:10 +0530

    My auto-build script
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+# SET SOME PATH VARIABLES
+ROOT="/home/aditya/i9003"
+# Toolchain path 
+CROSS_COMPILE="/home/aditya/Toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi"
+KERNEL_DIR="/home/aditya/i9003/GB"
+RAMDISK_DIR="/home/aditya/i9003/ramdisk"
+MODULES_DIR="$RAMDISK_DIR/lib/modules"
+OUT="/home/aditya/i9003/out"
+# Set correct base address as per device
+BASE="81800000"
+
+# DONE
+
+echo "|~~~~~~~~COMPILING TITANIUM KERNEL ~~~~~~~~~~~|"
+echo "|---------------------------------------------|"
+echo "Cleaning source"
+rm $OUT/normalboot.img
+cd ../
+rm *.gz
+cd out 
+cd $KERNEL_DIR 
+export CROSS_COMPILE=$CROSS_COMPILE-
+make -j clean mrproper
+echo "Importing defconfig"
+make -j latona_galaxysl_defconfig
+echo "Please Enter Release Version" 
+read $version 
+echo $version > .version
+echo ">> COMPILING!"
+make -j84
+echo "Copying modules and stripping em"
+find -name '*.ko' -exec cp -av {} $MODULES_DIR/ \;
+
+
+cd $MODULES_DIR
+echo "Strip modules for size"
+
+for m in $(find . | grep .ko | grep './')
+do
+        echo $m
+
+/home/aditya/Toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi-strip --strip-unneeded $m
+done
+
+
+cd $KERNEL_DIR
+echo "Packing Ramdisk"
+cd $ROOT
+./mkbootfs $RAMDISK_DIR | lzma > ramdisk.lzma 
+echo "Pack normalboot"
+./mkbootimg --kernel $KERNEL_DIR/arch/arm/boot/zImage --ramdisk ramdisk.lzma -o $OUT/normalboot.img --base $BASE
+
+cd $OUT
+echo "Clear old tarballs"
+rm *.tar
+
+tar -cvf KERNEL.tar normalboot.img
+
+
+echo "DONE, PRESS ENTER TO FINISH"
+read ANS
+ 
