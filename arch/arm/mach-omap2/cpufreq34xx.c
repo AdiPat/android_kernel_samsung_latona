aaecea03417661a063a463ffdf968ef0a4e15f87
AdiPat <adithemagnificent@gmail.com>
2012-11-14 20:23:06 +0530

    Revert "omap2: Undervolt stock Frequencies"
    This reverts commit 786c1c818da819c07c8fee447b69a51ca207e1b7.

--- arch/arm/mach-omap2/cpufreq34xx.c
+++ arch/arm/mach-omap2/cpufreq34xx.c
@@ -82,13 +82,13 @@ static u32 omap34xx_opp_def_size = ARRAY_SIZE(omap34xx_opp_def_list);
 
 static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 	/* MPU OPP1 - OPP50 */
-	OMAP_OPP_DEF("mpu", true,  300000000,  925000), 
+	OMAP_OPP_DEF("mpu", true,  300000000,  1025000), 
 	/* MPU OPP2 - OPP100 */
-	OMAP_OPP_DEF("mpu", true,  600000000,  1170000),   
+	OMAP_OPP_DEF("mpu", true,  600000000,  1200000),   
 	/* MPU OPP3 - OPP-Turbo */
-	OMAP_OPP_DEF("mpu", true,  800000000,   1300000),
+	OMAP_OPP_DEF("mpu", true,  800000000,   1330000),
 	/* MPU OPP4 - OPP-SB */
-	OMAP_OPP_DEF("mpu", true,  1000000000, 1340500),
+	OMAP_OPP_DEF("mpu", true,  1000000000, 1387500),
 
 	/* L3 OPP1 - OPP50 */
 	OMAP_OPP_DEF("l3_main", false, 100000000, 930000),     
@@ -96,13 +96,13 @@ static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 	OMAP_OPP_DEF("l3_main", true, 200000000, 1162500),
 
 	/* DSP OPP1 - OPP50 */
-	OMAP_OPP_DEF("iva", true,  260000000, 925000),
+	OMAP_OPP_DEF("iva", true,  260000000, 1025000),
 	/* DSP OPP2 - OPP100 */
-	OMAP_OPP_DEF("iva", true,  520000000, 1170000),
+	OMAP_OPP_DEF("iva", true,  520000000, 1200000),
 	/* DSP OPP3 - OPP-Turbo */
-	OMAP_OPP_DEF("iva", true, 660000000, 1300000),
+	OMAP_OPP_DEF("iva", true, 660000000, 1330000),
 	/* DSP OPP4 - OPP-SB */
-	OMAP_OPP_DEF("iva", true, 800000000, 1340500),
+	OMAP_OPP_DEF("iva", true, 800000000, 1387500),
 };
 static u32 omap36xx_opp_def_size = ARRAY_SIZE(omap36xx_opp_def_list);
 
