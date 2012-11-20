be127366577523160529287cda267dc2fe145ce8
AdiPat <adithemagnificent@gmail.com>
2012-11-20 22:20:08 +0530

    mach-omap2-mux: Goodbye.. 2 more section mismatches fixed!
    Darned samsung sec code-
    
    WARNING: vmlinux.o(.data+0xe478): Section mismatch in reference from the variable sec_board_wakeup_gpio_ptr to the (unknown reference) .init.data:(unknown)
    The variable sec_board_wakeup_gpio_ptr references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: vmlinux.o(.data+0xe480): Section mismatch in reference from the variable sec_board_output_gpio_ptr to the (unknown reference) .init.data:(unknown)
    The variable sec_board_output_gpio_ptr references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

--- arch/arm/mach-omap2/mux_latona_rev_r08.c
+++ arch/arm/mach-omap2/mux_latona_rev_r08.c
@@ -48,7 +48,7 @@
 
 #define SEC_OMAP_OUTPUT_GPIO(name, val)		{name, val, (unsigned int)#name},
 
-static unsigned int __omap_board_output_gpio[][3] __initdata = {
+static unsigned int __omap_board_output_gpio[][3] = {
 	SEC_OMAP_OUTPUT_GPIO(OMAP_GPIO_PS_HOLD_PU, 0)
 	SEC_OMAP_OUTPUT_GPIO(OMAP_GPIO_FM_nRST, 0)
 	SEC_OMAP_OUTPUT_GPIO(OMAP_GPIO_CAM_CIF_NRST, 0)
@@ -92,7 +92,7 @@ EXPORT_SYMBOL(sec_board_output_gpio_size);
 unsigned int (*sec_board_output_gpio_ptr)[3] = __omap_board_output_gpio;
 EXPORT_SYMBOL(sec_board_output_gpio_ptr);
 
-static unsigned int __omap_wakeup_gpio[] __initdata = {
+static unsigned int __omap_wakeup_gpio[] = {
 	OMAP_GPIO_JACK_NINT,
 	OMAP_GPIO_IPC_SRDY,
 	OMAP_GPIO_CHG_ING_N,
