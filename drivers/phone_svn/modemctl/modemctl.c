95a49666739e59ec12e42e162474f518d8812cd4
sconosciuto <sconosciuto.git@gmail.com>
2012-11-29 17:37:03 +0100

    Reworked sync bug fix This requires CONFIG_SVNET_WHITELIST to be unset. Otherwise the former fix will be used.

--- drivers/phone_svn/modemctl/modemctl.c
+++ drivers/phone_svn/modemctl/modemctl.c
@@ -47,14 +47,16 @@
 #endif
 
 #ifdef CONFIG_SVNET_WHITELIST
-#include <linux/phone_svn/portlist.h>
+#include "portlist.h"
 #endif /* CONFIG_SVNET_WHITELIST */
 
 #define DRVNAME "modemctl"
 
 //#define USE_EARLYSUSPEND_TO_CTRL_PDAACTIVE_LOW
-/* #define USE_LATERESUME_TO_CTRL_PDAACTIVE_HIGH */
-//#define USE_EARLYSUSPEND_TO_CTRL_WHITELIST
+#ifndef CONFIG_SVNET_WHITELIST
+    #define USE_LATERESUME_TO_CTRL_PDAACTIVE_HIGH
+#endif
+#define USE_EARLYSUSPEND_TO_CTRL_WHITELIST
 
 //#define SIM_DEBOUNCE_TIME_HZ	(HZ)
 
@@ -960,7 +962,6 @@ static int modemctl_suspend(struct platform_device *pdev, pm_message_t state)
 	pda_off(mc);
 #endif
 
-#if 0
 #if defined( CONFIG_SVNET_WHITELIST ) && \
 	!defined( USE_EARLYSUSPEND_TO_CTRL_WHITELIST )
 	// call process white list
@@ -968,7 +969,6 @@ static int modemctl_suspend(struct platform_device *pdev, pm_message_t state)
 	if( unlikely( retval !=0 ) ) {
 		printk( "fail to send whitelist : %d\n", retval );
 	} 
-#endif
 #endif
 
 	return 0;
@@ -1020,4 +1020,3 @@ module_exit(modemctl_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Suchang Woo <suchang.woo@samsung.com>");
 MODULE_DESCRIPTION("Modem control");
-
